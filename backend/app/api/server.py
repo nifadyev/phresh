c196fb7fcc6cee8ce49863f1ea299199724c1eba
Vadim Nifadev <drcs_velde@simplelogin.com>
2023-02-11 12:40:37 +0100

    Move app configuration to specific file

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from app.core import config, tasks
 from app.api.routes import router as api_router
 
 
 def get_application() -> FastAPI:
-    app = FastAPI(title="Phresh", version="1.0.0")
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
@@ -13,6 +14,10 @@ def get_application() -> FastAPI:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
+
     app.include_router(api_router, prefix="/api")
 
     return app
