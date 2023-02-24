2ccd60a9cc308865706a60c3dc5d368239ce06b7
Vadim Nifadev <drcs_velde@simplelogin.com>
2023-02-24 11:46:11 +0100

    Add users related endpoints, models and DB communication

--- backend/app/models/core.py
+++ backend/app/models/core.py
@@ -1,8 +1,19 @@
-from pydantic import BaseModel
+from datetime import datetime
+
+from pydantic import BaseModel, validator
 
 
 class CoreModel(BaseModel):
-    pass
+    """Any common logic to be shared by all models goes here."""
+
+
+class DateTimeModelMixin(BaseModel):
+    created_at: datetime | None
+    updated_at: datetime | None
+
+    @validator("created_at", "updated_at", pre=True)
+    def default_datetime(cls, value: datetime) -> datetime:
+        return value or datetime.datetime.now()
 
 
 class IDModelMixin(BaseModel):
