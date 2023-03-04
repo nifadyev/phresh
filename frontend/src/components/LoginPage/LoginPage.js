eaf903612df8e88701d30ad92f9647ab0ec4d6e7
Vadim Nifadev <drcs_velde@simplelogin.com>
2023-03-04 10:02:54 +0100

    Implement login page with login form and trivial validation

--- frontend/src/components/LoginPage/LoginPage.js
+++ frontend/src/components/LoginPage/LoginPage.js
@@ -1,5 +1,47 @@
 import React from "react"
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent_Deprecated,
+  EuiPageContentBody_Deprecated,
+  EuiPageHeader,
+  EuiPageHeaderSection,
+  EuiTitle
+} from "@elastic/eui"
+import { LoginForm } from "../../components"
+import styled from "styled-components"
+
+const StyledEuiPage = styled(EuiPage)`
+  flex: 1;
+`
+const StyledEuiPageHeader = styled(EuiPageHeader)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem;
+
+  & h1 {
+    font-size: 3.5rem;
+  }
+`
 
 export default function LoginPage() {
-  return <h1>Login</h1>
+  return (
+    <StyledEuiPage>
+      <EuiPageBody component="section">
+        <StyledEuiPageHeader>
+          <EuiPageHeaderSection>
+            <EuiTitle size="l">
+              <h1>Login</h1>
+            </EuiTitle>
+          </EuiPageHeaderSection>
+        </StyledEuiPageHeader>
+        <EuiPageContent_Deprecated verticalPosition="center" horizontalPosition="center">
+          <EuiPageContentBody_Deprecated>
+            <LoginForm />
+          </EuiPageContentBody_Deprecated>
+        </EuiPageContent_Deprecated>
+      </EuiPageBody>
+    </StyledEuiPage>
+  )
 }
