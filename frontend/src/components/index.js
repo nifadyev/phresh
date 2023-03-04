073401fb826b4c7dba75f02298a887c3136e8ae3
Vadim Nifadev <drcs_velde@simplelogin.com>
2023-03-04 09:37:35 +0100

    Add source index

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,1 +1,6 @@
 export { default as App } from "./App/App"
+export { default as Carousel } from "./Carousel/Carousel"
+export { default as CarouselTitle } from "./CarouselTitle/CarouselTitle"
+export { default as LandingPage } from "./LandingPage/LandingPage"
+export { default as Layout } from "./Layout/Layout"
+export { default as Navbar } from "./Navbar/Navbar"
