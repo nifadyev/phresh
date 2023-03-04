014f9a14e8dd0fba0e605374a1a9eb89c0b16abc
Vadim Nifadev <drcs_velde@simplelogin.com>
2023-03-04 10:11:56 +0100

    Implement registration page

--- frontend/src/components/RegistrationPage/RegistrationPage.js
+++ frontend/src/components/RegistrationPage/RegistrationPage.js
@@ -1,5 +1,47 @@
 import React from "react"
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent_Deprecated,
+  EuiPageContentBody_Deprecated,
+  EuiPageHeader,
+  EuiPageHeaderSection,
+  EuiTitle
+} from "@elastic/eui"
+import { RegistrationForm } from "../../components"
+import styled from "styled-components"
 
-export default function RegistrationPage(){
-  return <h1>Registration</h1>
+const StyledEuiPage = styled(EuiPage)`
+  flex: 1;
+`
+const StyledEuiPageHeader = styled(EuiPageHeader)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem;
+
+  & h1 {
+    font-size: 3.5rem;
+  }
+`
+
+export default function RegistrationPage() {
+  return (
+    <StyledEuiPage>
+      <EuiPageBody component="section">
+        <StyledEuiPageHeader>
+          <EuiPageHeaderSection>
+            <EuiTitle size="l">
+              <h1>Sign Up</h1>
+            </EuiTitle>
+          </EuiPageHeaderSection>
+        </StyledEuiPageHeader>
+        <EuiPageContent_Deprecated verticalPosition="center" horizontalPosition="center">
+          <EuiPageContentBody_Deprecated>
+            <RegistrationForm />
+          </EuiPageContentBody_Deprecated>
+        </EuiPageContent_Deprecated>
+      </EuiPageBody>
+    </StyledEuiPage>
+  )
 }
