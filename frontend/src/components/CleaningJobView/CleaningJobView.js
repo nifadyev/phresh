61cc9e71c77e2eeb36ebb40efa4e15f3c1b7214e
Vadim Nifadev <drcs_velde@simplelogin.com>
2023-03-10 10:20:05 +0100

    Show offers count on cleaning job detail view

--- frontend/src/components/CleaningJobView/CleaningJobView.js
+++ frontend/src/components/CleaningJobView/CleaningJobView.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Routes, Route, useNavigate } from "react-router-dom"
 import { connect } from "react-redux"
 import { Actions as cleaningActions } from "../../redux/cleanings"
+import { Actions as offersActions } from "../../redux/offers"
 import {
   EuiAvatar,
   EuiButtonEmpty,
@@ -34,27 +35,42 @@ const StyledFlexGroup = styled(EuiFlexGroup)`
 function CleaningJobView({
   user,
   isLoading,
+  offersError,
   cleaningError,
+  offersIsLoading,
   currentCleaningJob,
   fetchCleaningJobById,
-  clearCurrentCleaningJob
+  createOfferForCleaning,
+  clearCurrentCleaningJob,
+  fetchUserOfferForCleaningJob
 }) {
   const { cleaning_id } = useParams()
   const navigate = useNavigate()
 
+  const userOwnsCleaningResource = user?.username && currentCleaningJob?.owner?.id === user?.id
+
   React.useEffect(() => {
-    if (cleaning_id) {
+    if (cleaning_id && user?.username) {
       fetchCleaningJobById({ cleaning_id })
+
+      if (!userOwnsCleaningResource) {
+        fetchUserOfferForCleaningJob({ cleaning_id, username: user.username })
+      }
     }
 
     return () => clearCurrentCleaningJob()
-  }, [cleaning_id, fetchCleaningJobById, clearCurrentCleaningJob])
+  }, [
+    cleaning_id,
+    fetchCleaningJobById,
+    clearCurrentCleaningJob,
+    userOwnsCleaningResource,
+    fetchUserOfferForCleaningJob,
+    user
+  ])
 
   if (isLoading) return <EuiLoadingSpinner size="xl" />
   if (!currentCleaningJob) return <EuiLoadingSpinner size="xl" />
   if (!currentCleaningJob?.name) return <NotFoundPage />
-
-  const userOwnsCleaningResource = currentCleaningJob?.owner?.id === user?.id
 
   const editJobButton = userOwnsCleaningResource ? (
     <EuiButtonIcon iconType="documentEdit" aria-label="edit" onClick={() => navigate(`edit`)} />
@@ -69,6 +85,16 @@ function CleaningJobView({
     </EuiButtonEmpty>
   )
 
+  const viewCleaningJobElement = (
+    <CleaningJobCard
+      user={user}
+      offersError={offersError}
+      offersIsLoading={offersIsLoading}
+      cleaningJob={currentCleaningJob}
+      isOwner={userOwnsCleaningResource}
+      createOfferForCleaning={createOfferForCleaning}
+    />
+  )
   const editCleaningJobElement = (
     <PermissionsNeeded
       element={<CleaningJobEditForm cleaningJob={currentCleaningJob} />}
@@ -117,7 +143,7 @@ function CleaningJobView({
 
           <EuiPageContentBody_Deprecated>
             <Routes>
-              <Route path="/" element={<CleaningJobCard cleaningJob={currentCleaningJob} />} />
+              <Route path="/" element={viewCleaningJobElement} />
               <Route path="/edit" element={editCleaningJobElement} />
               <Route path="*" element={<NotFoundPage />} />
             </Routes>
@@ -132,11 +158,15 @@ export default connect(
   (state) => ({
     user: state.auth.user,
     isLoading: state.cleanings.isLoading,
+    offersIsLoading: state.offers.isLoading,
+    offersError: state.offers.error,
     cleaningError: state.cleanings.cleaningsError,
     currentCleaningJob: state.cleanings.currentCleaningJob
   }),
   {
     fetchCleaningJobById: cleaningActions.fetchCleaningJobById,
-    clearCurrentCleaningJob: cleaningActions.clearCurrentCleaningJob
+    clearCurrentCleaningJob: cleaningActions.clearCurrentCleaningJob,
+    fetchUserOfferForCleaningJob: offersActions.fetchUserOfferForCleaningJob,
+    createOfferForCleaning: offersActions.createOfferForCleaning
   }
 )(CleaningJobView)
