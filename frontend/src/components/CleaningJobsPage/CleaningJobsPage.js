9cb02efed2a5bc07c9acd9a768dc23b1887b3bed
Vadim Nifadev <drcs_velde@simplelogin.com>
2023-03-13 10:04:25 +0100

    Change imports

--- frontend/src/components/CleaningJobsPage/CleaningJobsPage.js
+++ frontend/src/components/CleaningJobsPage/CleaningJobsPage.js
@@ -1,13 +1,13 @@
 import React from "react"
-import { CleaningJobsHome, CleaningJobView, NotFoundPage } from "../../components"
 import { Routes, Route } from "react-router-dom"
+import { CleaningJobsHome, CleaningJobView, NotFoundPage } from "components"
 
 export default function CleaningJobsPage() {
   return (
     <>
       <Routes>
         <Route path="/" element={<CleaningJobsHome />} />
-        <Route path=":cleaning_id/*" element={<CleaningJobView />} />
+        <Route path=":cleaningId/*" element={<CleaningJobView />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
